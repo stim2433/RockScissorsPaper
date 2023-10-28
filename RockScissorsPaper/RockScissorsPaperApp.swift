be707805995bca37394f604ac75d1d8c5c6f3b78
stimLite <gross2433@gmail.com>
2023-10-28 18:33:11 +0300

    Initial Commit

---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by stimLite on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
